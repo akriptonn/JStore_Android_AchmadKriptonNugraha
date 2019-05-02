4848bacbb7bf563c40645c6cf60c2e38686b4a9c
Kripton <tonkripton@gmail.com>
2019-05-02 15:37:25 +0700

    Modul 9 TP

---
+++ app/src/main/java/com/example/jstore_android_achmadkriptonnugraha/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.example.jstore_android_achmadkriptonnugraha;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class LoginActivity extends AppCompatActivity {
+    final String constEmail = "test@test.com";
+    final String constPassword = "test";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText emailInput = (EditText) findViewById(R.id.editText);
+        final EditText passInput = (EditText) findViewById(R.id.editText2);
+        final Button loginButton = (Button) findViewById(R.id.button);
+        final TextView registerClickable = (TextView) findViewById(R.id.textView);
+
+        registerClickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regisIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(regisIntent);
+            }
+        });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = emailInput.getText().toString();
+                final String password = passInput.getText().toString();
+//                AlertDialog.Builder builder2 = new AlertDialog.Builder(LoginActivity.this);
+//                builder2.setMessage(email+" "+password).create().show();
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if (jsonResponse != null) {
+                                AlertDialog.Builder builder1 = new AlertDialog.Builder(LoginActivity.this);
+                                builder1.setMessage("Login Success").create().show();
+                            }
+                        }
+                        catch (JSONException e)
+                        {
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(LoginActivity.this);
+                            builder1.setMessage("Login Failed").create().show();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(email,password,responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/jstore_android_achmadkriptonnugraha/RegisterActivity.java
@@ -0,0 +1,62 @@
+package com.example.jstore_android_achmadkriptonnugraha;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText emailInput = (EditText) findViewById(R.id.editText);
+        final EditText passInput = (EditText) findViewById(R.id.editText2);
+        final EditText nameInput = (EditText) findViewById(R.id.editText4);
+        final Button loginButton = (Button) findViewById(R.id.button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = emailInput.getText().toString();
+                final String password = passInput.getText().toString();
+                final String fullName = nameInput.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if (jsonResponse != null)
+                            {
+                                AlertDialog.Builder builder1 = new AlertDialog.Builder(RegisterActivity.this);
+                                builder1.setMessage("Register Success").create().show();
+                            }
+                        }catch (JSONException e)
+                        {
+                            AlertDialog.Builder builder1 = new AlertDialog.Builder(RegisterActivity.this);
+                            builder1.setMessage("Register Failed").create().show();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(fullName, email, password, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+
+            }
+        });
+
+    }
+}
